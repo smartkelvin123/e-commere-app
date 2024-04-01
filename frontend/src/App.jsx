075ef046c9c715067c50a75221159feb48c21173
unknown <smartkelvin66@gmail.com>
2024-04-01 14:37:03 +0100

    start-project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import react from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  BrowserRouter,
+} from "react-router-dom";
+import Home from "./component/Home";
+import Navbar from "./component/Navbar";
+import Cart from "./component/Cart";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
